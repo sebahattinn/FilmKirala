3be67346e7bb5980284fa2432b9cba85791cba46
sebahattinn <112737466+sebahattinn@users.noreply.github.com>
2026-02-18 11:33:14 +0300

    Bugfix : mass improvement made

---
+++ FilmKirala.Application/DTOs/UserCacheDtos.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilmKirala.Application.DTOs
+{
+    public record UserCacheDtos(
+        int Id,
+        string Username,
+        string Email,
+        int WalletBalance,
+        string Role
+    );
+}
